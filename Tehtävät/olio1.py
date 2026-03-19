0be6b9965454b0a46df270305f1b6322f46f0fa8
Oiva <oiva.paatsema@gmail.com>
2026-03-19 10:14:57 +0200

    Add files via upload

---
+++ Tehtävät/olio1.py
@@ -0,0 +1,17 @@
+class Auto:
+
+    def __init__(self, tunnus, nopeus):
+     self.tunnus = tunnus
+     self.nopeus = nopeus
+     self.nyt =0
+     self. nopeus_nyt =0
+
+if __name__ == '__main__':
+
+ Uusi_auto =Auto("ABC-123",142 )
+
+print("auto")
+print(f"tunnus {Uusi_auto.tunnus}")
+print(f"nopeus {Uusi_auto.nopeus}")
+print(f"nyt {Uusi_auto.nyt}")
+print(f"matka {Uusi_auto.nopeus_nyt}")
