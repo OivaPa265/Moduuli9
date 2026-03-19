7bd13414c0da5ade693c82b025cd25ecb3a86756
Oiva <oiva.paatsema@gmail.com>
2026-03-19 13:22:05 +0200

    Update olio2.py
    Reformatted and added task description

--- Tehtävät/olio2.py
+++ Tehtävät/olio2.py
@@ -1,3 +1,8 @@
+#Jatka ohjelmaa kirjoittamalla Auto-luokkaan kiihdytä-metodi, joka saa parametrinaan nopeuden muutoksen (km/h).
+# Jos nopeuden muutos on negatiivinen, auto hidastaa. Metodin on muutettava auto-olion nopeus-ominaisuuden arvoa. 
+# Auton nopeus ei saa kasvaa huippunopeutta suuremmaksi eikä alentua nollaa pienemmäksi. Jatka pääohjelmaa siten, että auton nopeutta nostetaan ensin +30 km/h, sitten +70 km/h ja lopuksi +50 km/h. 
+# Tulosta tämän jälkeen auton nopeus. Tee sitten hätäjarrutus määräämällä nopeuden muutos -200 km/h ja tulosta uusi nopeus. 
+# Kuljettua matkaa ei tarvitse vielä päivittää.
 
 # luo luokan auto
 class Auto:
@@ -11,13 +16,11 @@ class Auto:
     def kiihdyta(self, kiihdytys):
         # tekee kiihtyvyyden laskun ja varmistaa ettei mene alle 0
         self.nopeus_nyt = max(0, min(self.nopeus, self.nopeus_nyt+kiihdytys))
-
         # printaa lisätävän kiihtyvyyden sekä nopeuden
         print(f"kiihdytys {kiihdytys:}  nopeus: {self.nopeus_nyt} ")
 
 # Antaa auto luokalle annetun tunnuksen sekä nopeuden
 auto = Auto("ABC-123", 142 )
-
 # printaa auton nopeuden eri kiihdytys vaiheissa
 print("kiihdykset")
 auto.kiihdyta(30)
